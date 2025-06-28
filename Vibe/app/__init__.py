56e9ece40160a2d4c07b668a2ff76a5de4d9a72a
Hrishi Mehta <112641614+mehtahrishi@users.noreply.github.com>
2025-06-28 06:08:02 +0530

    Update __init__.py

--- Vibe/app/__init__.py
+++ Vibe/app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 def create_app():
     """Create a minimal Flask application."""
-    app = Flask(__name__)
+   
+    app = Flask(__name__, template_folder='templates', static_folder='static')
+
 
     # No database or complex config needed anymore
     
